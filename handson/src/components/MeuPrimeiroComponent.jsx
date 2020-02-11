2c7cb8428f49b66b091bf5d1af758b90e1260f78
TIOZAO <marco_aurelioo@yahoo.com.br>
2020-02-10 23:08:20 -0300

    aula 1

---
+++ handson/src/components/MeuPrimeiroComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function primeiro(){
+    return <h1>meu component</h1>
+}
+
+export default primeiro
---
+++ handson/src/index.js
@@ -0,0 +1,8 @@
+import  React from 'react'
+import ReactDOM from 'react-dom'
+import MeuPrimeiroComponent from './components/MeuPrimeiroComponent'
+
+const element = document.getElementById('root')
+
+ReactDOM.render(
+<MeuPrimeiroComponent />, element)
